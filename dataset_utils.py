1578b2c49399a51753d04c61c7401f017bc95cfe
Vinoth Pandian <vinoth@dell.fritz.box>
2018-09-26 12:14:48 +0200

    raccoon test successful

---
+++ dataset_utils.py
@@ -0,0 +1,31 @@
+import numpy as np
+import os
+import shutil
+
+arr = np.arange(1, 200)
+
+np.random.shuffle(arr)
+
+train, val = arr[:159], arr[159:]
+
+img_path = "./raccoon_dataset/images/"
+anno_path = "./raccoon_dataset/annotations/"
+
+train_img_path = "./train_img/"
+train_anno_path = "./train_anno/"
+
+val_img_path = "./val_img/"
+val_anno_path = "./val_anno/"
+
+for i in train:
+    shutil.copy(os.path.join(
+        img_path, "raccoon-{}.jpg".format(i)), train_img_path)
+    shutil.copy(os.path.join(
+        anno_path, "raccoon-{}.xml".format(i)), train_anno_path)
+
+
+for i in val:
+    shutil.copy(os.path.join(
+        img_path, "raccoon-{}.jpg".format(i)), val_img_path)
+    shutil.copy(os.path.join(
+        anno_path, "raccoon-{}.xml".format(i)), val_anno_path)
